1ddd8878b1ae3d3d4653dd90424eaa241db65a23
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2019-11-22 14:19:30 +0100

    Type ClusterIP missing.

--- setup/create_db_host_svc.sh
+++ setup/create_db_host_svc.sh
@@ -3,24 +3,24 @@ SCRIPTPATH=$(dirname $0)
 #
 . $SCRIPTPATH/oke_env.sh
 echo Create Service for medrecdbhostname
-cat << EOF | kubectl apply -f -
+cat <<EOF | kubectl apply -f -
 kind: Service
 apiVersion: v1
 metadata:
- name: medrecdbhostname
+name: medrecdbhostname
 spec:
- ports:
- - port: 1521
-   targetPort: 1521
+type: ClusterIP
+ports:
+- port: 1521
+  targetPort: 1521
 ---
 kind: Endpoints
 apiVersion: v1
 metadata:
- name: medrecdbhostname
+name: medrecdbhostname
 subsets:
- - addresses:
-     - ip: 10.0.10.6
-   ports:
-     - port: 1521
+- addresses:
+    - ip: 10.0.10.6
+  ports:
+    - port: 1521
 EOF
-
