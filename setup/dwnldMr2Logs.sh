522e1e5c9e02efeab61ed39c17ee408645bc4b36
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2019-11-29 21:18:20 +0100

    MR2_POD

--- setup/dwnldMr2Logs.sh
+++ setup/dwnldMr2Logs.sh
@@ -6,5 +6,5 @@ SCRIPTPATH=$(dirname $0)
 LOG_FILE=$MR_SVR2.log
 OUT_FILE=$MR_SVR2.out
 #
-echo From $WLS_DMN_NS/$MR1_POD download $DMN_HOME/servers/$MR_SVR2/logs/$LOG_FILE to $LCL_LOGS_HOME/$LOG_FILE
-kubectl cp $WLS_DMN_NS/$MR1_POD:$DMN_HOME/servers/$MR_SVR2/logs/$LOG_FILE $LCL_LOGS_HOME/$LOG_FILE
+echo From $WLS_DMN_NS/$MR2_POD download $DMN_HOME/servers/$MR_SVR2/logs/$LOG_FILE to $LCL_LOGS_HOME/$LOG_FILE
+kubectl cp $WLS_DMN_NS/$MR2_POD:$DMN_HOME/servers/$MR_SVR2/logs/$LOG_FILE $LCL_LOGS_HOME/$LOG_FILE
