a2ba0d067e0cddf5efbd482c31b021cae0e2ceaf
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2019-12-01 15:01:09 +0100

    Adding the outfiles downloads.

--- setup/dwnldAdmLogs.sh
+++ setup/dwnldAdmLogs.sh
@@ -8,3 +8,5 @@ OUT_FILE=$ADM_SVR.out
 #
 echo From $WLS_DMN_NS/$MR1_POD download $DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE to $LCL_LOGS_HOME/$LOG_FILE
 kubectl cp $WLS_DMN_NS/$MR1_POD:$DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE $LCL_LOGS_HOME/$LOG_FILE
+echo From $WLS_DMN_NS/$MR1_POD download $DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE to $LCL_LOGS_HOME/$OUT_FILE
+kubectl cp $WLS_DMN_NS/$MR1_POD:$DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE $LCL_LOGS_HOME/$OUT_FILE
