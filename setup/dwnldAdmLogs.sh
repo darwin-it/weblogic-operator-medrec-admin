07499890282a7335dc5228217615a3feb4c25904
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2019-12-01 15:14:56 +0100

    Change MR1_POD to ADM_POD.

--- setup/dwnldAdmLogs.sh
+++ setup/dwnldAdmLogs.sh
@@ -6,7 +6,7 @@ SCRIPTPATH=$(dirname $0)
 LOG_FILE=$ADM_SVR.log
 OUT_FILE=$ADM_SVR.out
 #
-echo From $WLS_DMN_NS/$MR1_POD download $DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE to $LCL_LOGS_HOME/$LOG_FILE
-kubectl cp $WLS_DMN_NS/$MR1_POD:$DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE $LCL_LOGS_HOME/$LOG_FILE
-echo From $WLS_DMN_NS/$MR1_POD download $DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE to $LCL_LOGS_HOME/$OUT_FILE
-kubectl cp $WLS_DMN_NS/$MR1_POD:$DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE $LCL_LOGS_HOME/$OUT_FILE
+echo From $WLS_DMN_NS/$ADM_POD download $DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE to $LCL_LOGS_HOME/$LOG_FILE
+kubectl cp $WLS_DMN_NS/$ADM_POD:$DMN_HOME/servers/$ADM_SVR/logs/$LOG_FILE $LCL_LOGS_HOME/$LOG_FILE
+echo From $WLS_DMN_NS/$ADM_POD download $DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE to $LCL_LOGS_HOME/$OUT_FILE
+kubectl cp $WLS_DMN_NS/$ADM_POD:$DMN_HOME/servers/$ADM_SVR/logs/$OUT_FILE $LCL_LOGS_HOME/$OUT_FILE
