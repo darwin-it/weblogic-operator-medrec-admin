a84a8209d0a84c0f01df47200fdd97ec24505cfa
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2020-02-21 13:22:07 +0100

    bijwerken scripts.

--- setup/getdmnpod-status.sh
+++ setup/getdmnpod-status.sh
@@ -2,6 +2,6 @@
 SCRIPTPATH=$(dirname $0)
 #
 . $SCRIPTPATH/oke_env.sh
-echo Get K8s pods for $WLS_DMN_NS
+echo Get K8s pod statuses for $WLS_DMN_NS
 kubectl -n $WLS_DMN_NS get pods -o wide| awk '{print $1 " - "  $3}'
 
--- setup/getdmnsitlogs.sh
+++ setup/getdmnsitlogs.sh
@@ -2,5 +2,5 @@
 SCRIPTPATH=$(dirname $0)
 #
 . $SCRIPTPATH/oke_env.sh
-echo Get situational config logs for $WLS_DMN_NS server medrec-domain-adminserver
-kubectl -n $WLS_DMN_NS logs medrec-domain-adminserver | grep -i situational
+echo Get situational config logs for $WLS_DMN_NS server $ADM_POD
+kubectl -n $WLS_DMN_NS logs $ADM_POD | grep -i situational
